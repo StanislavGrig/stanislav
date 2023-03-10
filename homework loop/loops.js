d9ba53b9dd32041a37122f5c607d719873470acb
StanislavGrig <123309191+StanislavGrig@users.noreply.github.com>
2023-03-10 13:23:20 +0400

    Add files via upload

---
+++ homework loop/loops.js
@@ -0,0 +1,63 @@
+//1)	Выведите чётные числа
+		//При помощи цикла for выведите чётные числа от 2 до 10.
+//Для проверки на чётность используйте оператор получения остатка от деления % - if (i % 2 == 0)
+
+
+//for(let i = 2; i <= 10; i++) {
+//if (i % 2 !== 0) {
+//console.log(i);
+//	}
+//}
+
+
+//1.1) Перепишите код, заменив цикл for на while, без изменения поведения цикла.
+// for (let i = 0; i < 3; i++) {
+		//   console.log( `number ${i}!` );
+		// }
+
+//let i = 0;
+//while(i < 3) {
+	  // console.log(i);
+	   //i++
+	//	}
+
+
+
+//2)
+
+		//Перепишите код в двух синтаксисах (одну можно закомментировать) Function Expression и стрелочной функцией
+		//Сейчас в функцию ask передаются анонимные каллбек функции, дайте им имена и передавайте как каллбек через переменную описания.
+//Function Expression:
+
+		//const ask=function(question, answer, yes, no) {
+		//  question();
+		//  if (answer) yes()
+		//  else no();
+		//}
+
+		//ask(
+		//  function() { console.log("Вы согласны?"); },
+		//  true,
+		 // function() { console.log("Вы согласились."); },
+		//  function() { console.log("Вы отказались."); }
+		//);
+
+
+// Стрелочная функция:
+
+const ask = (question, answer, yes, no) => {
+		  question();
+		  if (answer) yes()
+		  else no();
+		}
+
+
+		  const askConsole=()=>console.log("Вы согласны?"); 
+		 const  yesConsole=()=>console.log("Вы согласились."); 
+		 const noConsole=()=>console.log("Вы отказались.");
+		const answer = () => false;
+		 ask(
+		 askConsole,
+		 answer,
+		 yesConsole,
+		 noConsole);
